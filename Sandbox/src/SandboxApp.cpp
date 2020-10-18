78e6b16807bed820e080466f24c0baf02ae25635
Yan Lamothe <yan.santerre@gmail.com>
2020-10-18 01:00:51 -0400

    first commit

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,21 @@
+#include <GameEngine.h>
+
+class SandboxApp : public GameEngine::Application
+{
+public:
+	SandboxApp() 
+	{
+
+	}
+
+	~SandboxApp()
+	{
+
+	}
+
+};
+
+GameEngine::Application* GameEngine::CreateApplication()
+{
+	return new SandboxApp();
+}
